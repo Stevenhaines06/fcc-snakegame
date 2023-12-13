2d7b94a34affa0c8cef699829985d47f317a1c61
Stevenhaines06 <48706369+Stevenhaines06@users.noreply.github.com>
2023-12-12 23:50:01 -0500

    adding logic and elements

--- script.js
+++ script.js
@@ -2,8 +2,17 @@
 
 const board = document.getElementById("game-board");
 
+// Define Game variables
+
+let snake = [{ x: 10, y: 10 }]
+
 //Draw Game Map, snake, food
 function draw() {
     board.innerHTML = "";
     drawSnake();
+}
+
+// Draw Snake
+function drawSnake() {
+    snake.forEach
 }
