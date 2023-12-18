184889d32f4db9133cbe552c656f0d8c56f52db0
Stevenhaines06 <48706369+Stevenhaines06@users.noreply.github.com>
2023-12-17 23:55:48 -0500

    added more structure

--- script.js
+++ script.js
@@ -17,10 +17,24 @@ function draw() {
 function drawSnake() {
     snake.forEach((segment) => {
         const snakeElement = createGameElement('div', 'snake');
+        setPosition(snakeElement, segment)
+        board.appendChild(snakeElement)
     });
 }
 
 //Create a snake or food cube.div
+//className creates a div with a className of 'Snake'
 function createGameElement(tag, className) {
     const element = document.createElement(tag);
+    element.className = className;
+    return element;
 }
+
+//Set the position of the snake or the food
+function setPosition(element, position) {
+    element.style.gridColumn = position.x;
+    element.style.gridRow = position.y;
+}
+
+//Testing draw function
+draw();
